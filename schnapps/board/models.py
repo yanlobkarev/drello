02311377995734867bfd340dcb36623edd438e8c
Yan Lobkarev <yan.lobkarev@gmail.com>
2016-08-30 23:21:47 +0300

    + Status, Task models.

--- schnapps/board/models.py
+++ schnapps/board/models.py
@@ -1,5 +1,23 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField(blank=True, max_length=500)
+    status = models.ForeignKey('Status', on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.title
+
+
+class Status(models.Model):
+    class Meta:
+        verbose_name_plural = 'Statuses'
+
+    title = models.CharField(max_length=150)    
+
+    def __unicode__(self):
+        return self.title
+
+
